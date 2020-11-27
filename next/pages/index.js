8f03bed08b1370a8c8ec598317a2ef1ed2346250
Aleksey Belchenkov <u608110@gmail.com>
2020-11-27 16:23:55 +0300

    Fetch blog data

--- next/pages/index.js
+++ next/pages/index.js
@@ -4,26 +4,43 @@ import {
     Row
 } from "react-bootstrap";
 
+import { getAllBlogs } from 'lib/api';
 import PageLayout from "components/PageLayout";
 import AuthorIntro from "components/AuthorIntro";
 import CardListItem from "components/CardListItem";
 import CardItem from "components/CardItem";
 
-const Home = () => {
+const Home = ({blogs}) => {
     return (
         <PageLayout>
             <AuthorIntro />
             <hr/>
             <Row className="mb-5">
-                <Col md="10">
+                {/*<Col md="10">
                     <CardListItem />
-                </Col>
-                <Col md="4">
-                    <CardItem />
-                </Col>
+                </Col>*/}
+                {
+                    blogs.map(blog => (
+                        <Col key={blog.slug} md="4">
+                            <CardItem
+                                title={blog.title}
+                                subtitle={blog.subtitle}
+                            />
+                        </Col>
+                    ))
+                }
             </Row>
         </PageLayout>
     );
 };
+
+export async function getStaticProps() {
+    const blogs = await getAllBlogs();
+    return {
+        props: {
+            blogs
+        }
+    }
+}
 
 export default Home;
